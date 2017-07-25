eb041e9ebed8f1bb2713245aa68eaa2b731e8b21
igorag <igorag@mmtr.ru>
2017-07-25 18:41:54 +0300

    LWDEV-2249 Client search current version

--- src/Lykke.Service.ClientSearch.Services/FullTextSearch/PersonalDataLoader.cs
+++ src/Lykke.Service.ClientSearch.Services/FullTextSearch/PersonalDataLoader.cs
@@ -1,8 +1,11 @@
 ﻿using AzureStorage.Tables;
 using Common.Log;
 using Lykke.Service.ClientSearch.AzureRepositories.PersonalData;
+using Microsoft.WindowsAzure.Storage.Table;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Threading;
 
 namespace Lykke.Service.ClientSearch.FullTextSearch
@@ -10,6 +13,7 @@ namespace Lykke.Service.ClientSearch.FullTextSearch
     public class PersonalDataLoader
     {
         private static DateTimeOffset lastTimeStamp = DateTimeOffset.MinValue;
+        private static string[] lastLoadedEntites = null;
 
         public static void LoadAllAsync(string connectionString, string tableName, ILog log)
         {
@@ -46,23 +50,45 @@ namespace Lykke.Service.ClientSearch.FullTextSearch
             }
         }
 
-        public static void LoadNewRecords(string connectionString, string tableName, ILog log)
+        public static async void LoadNewRecords(string connectionString, string tableName, ILog log)
         {
             for (;;)
             {
                 try
                 {
                     AzureTableStorage<PersonalDataEntity> repo = new AzureTableStorage<PersonalDataEntity>(connectionString, tableName, log);
-                    IList<PersonalDataEntity> allPersonalData = repo.GetDataAsync(_ => _.Timestamp > lastTimeStamp).GetAwaiter().GetResult();
-                    foreach (PersonalDataEntity e in allPersonalData)
-                    {
-                        if (e.Timestamp > lastTimeStamp)
-                        {
-                            lastTimeStamp = e.Timestamp;
-                        }
-                    }
+                    // IList<PersonalDataEntity> allPersonalData = repo.GetDataAsync(_ => _.Timestamp > lastTimeStamp).GetAwaiter().GetResult();
 
-                    Indexer.CreateIndex(allPersonalData);
+                    string dt = lastTimeStamp.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                    var query = new TableQuery<PersonalDataEntity>
+                    {
+                        FilterString = $"Timestamp ge datetime'{dt}'"
+                    };
+                    IEnumerable<PersonalDataEntity> allPersonalData = await repo.WhereAsync(query);
+                    if (allPersonalData != null && allPersonalData.Count() > 0)
+                    {
+                        foreach (PersonalDataEntity e in allPersonalData)
+                        {
+                            if (e.Timestamp > lastTimeStamp)
+                            {
+                                lastTimeStamp = e.Timestamp;
+                            }
+                        }
+
+                        if (lastLoadedEntites == null)
+                        {
+                            lastLoadedEntites = allPersonalData.OrderBy(_ => _.Timestamp).Select(_ => _.Id + _.Timestamp.Ticks).ToArray();
+                        }
+                        else
+                        {
+                            if (lastLoadedEntites.SequenceEqual(allPersonalData.OrderBy(_ => _.Timestamp).Select(_ => _.Id + _.Timestamp.Ticks)))
+                            {
+                                lastTimeStamp = lastTimeStamp.AddSeconds(1);
+                            }
+                        }
+
+                        Indexer.CreateIndex(allPersonalData);
+                    }
                 }
                 catch
                 {
