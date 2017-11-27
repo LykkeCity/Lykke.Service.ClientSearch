b5e4ed2b14739d48323c9c1eecf55666839512c5
igorag <igorag@mmtr.ru>
2017-11-27 17:11:13 +0300

    LWDEV-3911 client search by fullname and date of birth - deposit method closed in controller

--- src/Lykke.Service.ClientSearch/Controllers/ClientFullTextSearchController.cs
+++ src/Lykke.Service.ClientSearch/Controllers/ClientFullTextSearchController.cs
@@ -24,6 +24,7 @@ namespace Lykke.Service.ClientSearch.Controllers
         {
         }
 
+        /*
         /// <summary>
         /// fulltext search for clients
         /// </summary>
@@ -33,6 +34,7 @@ namespace Lykke.Service.ClientSearch.Controllers
         {
             return Searcher.Search(requestItems);
         }
+        */
 
         [HttpPost]
         [Route("searchForExistingClient")]
