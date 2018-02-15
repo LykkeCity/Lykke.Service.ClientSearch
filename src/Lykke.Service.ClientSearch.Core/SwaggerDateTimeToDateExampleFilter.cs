84ba4cfc2ff1601e92098f0dc10f296480703725
igorag <igorag@mmtr.ru>
2018-02-15 16:54:54 +0300

    LWDEV-4063-Client-search-service

---
+++ src/Lykke.Service.ClientSearch.Core/SwaggerDateTimeToDateExampleFilter.cs
@@ -0,0 +1,23 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lykke.Service.ClientSearch
+{
+    public class SwaggerDateTimeToDateExampleFilter : ISchemaFilter
+    {
+        public void Apply(Schema model, SchemaFilterContext context)
+        {
+            foreach(var prop in model.Properties)
+            {
+                if (context.SystemType.GetProperty(prop.Key).PropertyType == typeof(DateTime))
+                {
+                    prop.Value.Example = DateTime.UtcNow.ToString("yyyy-MM-dd");
+                }
+            }
+        }
+    }
+
+}
